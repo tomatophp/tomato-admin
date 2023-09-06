91a638340cfa5b373ec695a62a9ab34c3334bcdd
Fady Mondy <EngFadyMondy@gmail.com>
2023-09-06 15:47:21 +0300

    fix menu translation

--- src/Services/Contracts/Menu.php
+++ src/Services/Contracts/Menu.php
@@ -56,9 +56,11 @@ class Menu
     public function __construct()
     {
         // decrypt
-        $decryptedString = \Crypt::decrypt(Cookie::get('lang'), false);
-        $lang = json_decode(explode('|', $decryptedString)[1]);
-        app()->setLocale($lang->id ?? config('app.locale'));
+        try {
+            $decryptedString = \Crypt::decrypt(Cookie::get('lang'), false);
+            $lang = json_decode(explode('|', $decryptedString)[1]);
+            app()->setLocale($lang->id ?? config('app.locale'));
+        }catch (\Exception $exception) {}
     }
 
 
