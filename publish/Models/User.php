4ee66a543b51d45713dd89586e3d073049f4b0e5
Fady Mondy <info@3x1.io>
2024-03-07 13:30:23 +0200

    update tomato-plugins and remove MustVerifyEmail from user model

--- publish/Models/User.php
+++ publish/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
  use Spatie\Permission\Traits\HasRoles;
 
- class User extends Authenticatable implements MustVerifyEmail
+ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     use HasTeams;
