cb4ff641b76b069fe698627eacc901c9ddf170e0
Fady Mondy <info@3x1.io>
2024-04-29 15:36:34 +0300

    update style

--- resources/views/components/markdown-editor.blade.php
+++ resources/views/components/markdown-editor.blade.php
@@ -1,8 +1,8 @@
 <label>
     <span class="block mb-1 text-zinc-700 font-sans dark:text-white">{{$label ?? ''}}</span>
 </label>
-<MdEditor {{ $attributes->only(['v-if', 'v-show', 'class'])->class(['hidden' => $isHidden()]) }}
-          v-model="{{ $vueModel() }}" language="en-US" :preview="@js($preview)" :toolbars="[
+<MdEditor  previewTheme="github" theme="dark" {{ $attributes->only(['v-if', 'v-show', 'class'])->class(['hidden' => $isHidden()]) }}
+v-model="{{ $vueModel() }}" language="en-US" :preview="@js($preview)" :toolbars="[
               'bold',
               'underline',
               'italic',
--- resources/views/components/markdown-viewer.blade.php
+++ resources/views/components/markdown-viewer.blade.php
@@ -1,3 +1,3 @@
 <div class="font-main">
-    <MdPreview editorId="preview-only" modelValue="{{$content}}" language="en-US" class="font-main" />
+    <MdPreview editorId="preview-only" modelValue="{{$content}}" language="en-US" {{ $attributes }} previewTheme="github" theme="dark" />
 </div>
